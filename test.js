5d31c7f7736c77763d5fc3c6ccf2fdcf847cc6fb
Dusty Jewett <dusty@patentnavigation.com>
2016-04-10 15:19:26 -0700

    Initial Commit

---
+++ test.js
@@ -0,0 +1,7 @@
+var inspect = require('util').inspect;
+var EmberR = require('ember');
+console.log("EmberR: ", EmberR.A(['Ember', 'Required']));
+
+import Ember from 'ember';
+
+console.log("Ember Import", Ember.A(['Ember', 'Imported']));
