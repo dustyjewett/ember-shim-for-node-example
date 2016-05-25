44e0139e665c8f1ab952d4fa3f18f78d844ce028
Dusty Jewett <dusty@patentnavigation.com>
2016-05-25 16:14:21 -0700

    Making the formatter work, adding comments.

--- process.js
+++ process.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
+import FormatDate from 'ember-intl/formatters/format-date';
 
+// A simple Ember Object with a computed property
 let ComputedObject = Ember.Object.extend({
     val:"value",
     computedValue: Ember.computed(function(){
@@ -7,11 +9,10 @@ let ComputedObject = Ember.Object.extend({
     })
 });
 
+// Computed Properties Work!
 let obj = ComputedObject.create({val:"the Value!"});
 console.log(obj.get('computedValue'));
 
-import FormatDate from 'ember-intl/formatters/format-date';
-
+// Addons even work!
 let formatter = FormatDate.create();
-
-console.log(formatter.format(new Date(), {locale:"en-us"}));
+console.log(formatter.format(new Date(), null, {locale:"en-us"}));
